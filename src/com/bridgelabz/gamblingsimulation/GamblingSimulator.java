c415b1c37b9446c84a084eeb47f721f820c5f16d
akash6280 <akashsingh6280@gmail.com>
2021-08-26 00:56:18 +0530

    ADDED:To win or lose game

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
@@ -3,7 +3,20 @@ package com.bridgelabz.gamblingsimulation;
 public class GamblingSimulator {
 	public static final int INITIAL_STAKE_EVERYDAY = 100;
 	public static final int BET_EVERYGAME = 1;
-	public static void main(String[] args) {
-		
+	public static final int WIN=1;
+	public static final int lose=0;
+	
+	public static int play(int stake) {
+		int option=(int)Math.floor(Math.random()*10)%2;
+		if(option==WIN)
+			stake++;
+		else
+			stake--;
+		return stake;
 	}
+    public static void main(String[] args) {
+	
+    	int finalStake=play(INITIAL_STAKE_EVERYDAY);
+    	System.out.println(finalStake);
+}
 }
