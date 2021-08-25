f0af77629eec22245b874d08de0c94b03fa27dc0
akash6280 <akashsingh6280@gmail.com>
2021-08-26 00:50:45 +0530

    ADDED: Initial Files

---
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblingsimulation;
+
+public class GamblingSimulator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to gambling simulation");
+	}
+}
