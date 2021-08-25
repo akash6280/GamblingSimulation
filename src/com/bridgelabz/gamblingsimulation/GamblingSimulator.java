36ddb8be5bfc974cc1858d7ea3420826a71ae6bd
akash6280 <akashsingh6280@gmail.com>
2021-08-26 00:58:06 +0530

    ADDED:Exit game with 50% lose or win

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
@@ -4,7 +4,9 @@ public class GamblingSimulator {
 	public static final int INITIAL_STAKE_EVERYDAY = 100;
 	public static final int BET_EVERYGAME = 1;
 	public static final int WIN=1;
-	public static final int lose=0;
+	public static final int Lose=0;
+	public static final int minStake = (INITIAL_STAKE_EVERYDAY)-(50*INITIAL_STAKE_EVERYDAY/100);
+	public static final int maxStake = (INITIAL_STAKE_EVERYDAY)+(50*INITIAL_STAKE_EVERYDAY/100);
 	
 	public static int play(int stake) {
 		int option=(int)Math.floor(Math.random()*10)%2;
@@ -14,9 +16,17 @@ public class GamblingSimulator {
 			stake--;
 		return stake;
 	}
+	public static int calculativeGambler() {
+		int balance=INITIAL_STAKE_EVERYDAY;
+		while(balance>minStake && balance<maxStake) {
+			balance=play(balance);
+		}
+		return balance;
+		
+	}
     public static void main(String[] args) {
 	
-    	int finalStake=play(INITIAL_STAKE_EVERYDAY);
+    	int finalStake=calculativeGambler();
     	System.out.println(finalStake);
 }
 }
