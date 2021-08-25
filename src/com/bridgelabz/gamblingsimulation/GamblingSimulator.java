7cfd85a3bfad65b1b64ef65bc96fd1e173a27606
akash6280 <akashsingh6280@gmail.com>
2021-08-26 00:59:46 +0530

    ADDED:Result after 20 days

--- src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulation/GamblingSimulator.java
@@ -7,6 +7,7 @@ public class GamblingSimulator {
 	public static final int Lose=0;
 	public static final int minStake = (INITIAL_STAKE_EVERYDAY)-(50*INITIAL_STAKE_EVERYDAY/100);
 	public static final int maxStake = (INITIAL_STAKE_EVERYDAY)+(50*INITIAL_STAKE_EVERYDAY/100);
+	public static final int NUMBER_OF_DAYS=20;
 	
 	public static int play(int stake) {
 		int option=(int)Math.floor(Math.random()*10)%2;
@@ -16,6 +17,7 @@ public class GamblingSimulator {
 			stake--;
 		return stake;
 	}
+	
 	public static int calculativeGambler() {
 		int balance=INITIAL_STAKE_EVERYDAY;
 		while(balance>minStake && balance<maxStake) {
@@ -24,9 +26,26 @@ public class GamblingSimulator {
 		return balance;
 		
 	}
+	public static int netReturn() {
+		int netAmount=0;
+		for(int day=1;day<=NUMBER_OF_DAYS;day++) {
+    		int balance=calculativeGambler();
+			if(balance>INITIAL_STAKE_EVERYDAY) {
+				netAmount+=balance-INITIAL_STAKE_EVERYDAY;
+			}
+			else {
+				netAmount-=INITIAL_STAKE_EVERYDAY-balance;
+			}
+	     }
+	    	return netAmount;
+	}
     public static void main(String[] args) {
-	
-    	int finalStake=calculativeGambler();
-    	System.out.println(finalStake);
+	    int netAmount=netReturn();
+    	if(netAmount>0)
+    		System.out.println("Player win amount = "+netAmount);
+    	else
+        	System.out.println("Player lose amount = "+Math.abs(netAmount));
+    		
 }
 }
+
